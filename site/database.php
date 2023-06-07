f93bad07e3160183f98ad2a657369a29fc108f74
Keiji <mikeykouw@hotmail.com>
2023-06-07 08:34:20 +0200

    Dag 1

---
+++ site/database.php
@@ -0,0 +1,8 @@
+// Database configuratie
+$host  = "localhost";
+$dbuser = "root";
+$dbpass = "password";
+$dbname = "";
+
+// Maak een  database connectie
+$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
